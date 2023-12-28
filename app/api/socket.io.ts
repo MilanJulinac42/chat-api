5d7a884abd7f4ad24cff7eabe7211149f778670c
milan julinac <milanjulinac996@gmail.com>
2023-12-28 18:50:06 +0100

    added error handling

--- app/api/socket.io.ts
+++ app/api/socket.io.ts
@@ -10,13 +10,23 @@ io.on("connection", (socket: socketIO.Socket) => {
     console.log("User connected");
 
     socket.on("join-chat", (roomName: string) => {
-        socket.join(roomName);
-        console.log(`User joined ${roomName}`);
+        try {
+            socket.join(roomName);
+            console.log(`User joined ${roomName}`);
+        } catch (error) {
+            console.error(`Error joining room ${roomName}:`, error);
+            socket.emit("join-error", "Failed to join room");
+        }
     });
 
     socket.on("chat-message", (message: string) => {
         const firstRoom = socket.rooms.values().next().value;
-        io.to(firstRoom).emit("chat-message", message);
+        try {
+            io.to(firstRoom).emit("chat-message", message);
+        } catch (error) {
+            console.error(`Error broadcasting message:`, error);
+            socket.emit("message-error", "Failed to send message");
+        }
     });
 
     socket.on("disconnect", () => {
