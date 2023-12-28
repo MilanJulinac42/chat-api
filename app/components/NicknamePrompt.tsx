c33a6a1646af372cc750cb206df9746ab3492a44
milan julinac <milanjulinac996@gmail.com>
2023-12-28 19:19:15 +0100

    nickanme prompt component added

---
+++ app/components/NicknamePrompt.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+interface NicknamePromptProps {
+    onJoin: (nickname: string) => void;
+}
+
+const NicknamePrompt: React.FC<NicknamePromptProps> = ({ onJoin }) => {
+    const [nickname, setNickname] = useState<string>("");
+
+    const handleJoin = () => {
+        if (nickname) {
+            onJoin(nickname);
+        } else {
+            alert("Please choose a meowgnificent nickname!");
+        }
+    };
+
+    return (
+        <div className="nickname-prompt">
+            <h2>Paws for a Mewment!</h2>
+            <p>Choose a pawsome nickname to enter the Cat Room:</p>
+            <input
+                type="text"
+                value={nickname}
+                onChange={(e) => setNickname(e.target.value)}
+            />
+            <button onClick={handleJoin}>Join the Cattitude!</button>
+        </div>
+    );
+};
+
+export default NicknamePrompt;
