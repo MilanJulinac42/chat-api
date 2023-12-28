ac145633a56541f03fab1413e1285448cd759fba
milan julinac <milanjulinac996@gmail.com>
2023-12-28 19:27:30 +0100

    added form

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import UserList from "./components/UserList";
 import NicknamePrompt from "./components/NicknamePrompt";
+import ChatBox from "./components/ChatBox";
 import io from "socket.io-client";
 
 const socket = io("http://localhost:3000");
@@ -9,6 +10,8 @@ const socket = io("http://localhost:3000");
 export default function Home() {
     const [users, setUsers] = useState<string[]>([]);
     const [showPrompt, setShowPrompt] = useState(true);
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState<string[]>([]);
 
     useEffect(() => {
         fetch("/api/users")
@@ -29,15 +32,38 @@ export default function Home() {
         };
     }, [socket]);
 
+    useEffect(() => {
+        socket.on("chat-message", (message: string) => {
+            setMessages((prevMessages) => [...prevMessages, message]);
+        });
+    }, [socket]);
+
     const handleJoin = (nickname: string) => {
         socket.emit("join-chat", "Cat Room");
         setShowPrompt(false);
+    };
+
+    const handleSendMessage = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        socket.emit("chat-message", message);
+        setMessage("");
     };
 
     return (
         <main className={styles.main}>
             {showPrompt && <NicknamePrompt onJoin={handleJoin} />}
             <UserList users={users} socket={socket} />
+            <ChatBox messages={messages} />
+
+            <form onSubmit={handleSendMessage}>
+                <input
+                    type="text"
+                    placeholder="Type your message..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button type="submit">Send</button>
+            </form>
         </main>
     );
 }
